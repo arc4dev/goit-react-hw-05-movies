c38059d770b9d48434f9fe53ca66482ea069c627
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-07-31 19:10:20 +0200

    small fix

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,8 +3,6 @@ import { Outlet } from 'react-router-dom';
 
 import { NavBar } from 'components/NavBar/NavBar';
 
-import css from './SharedLayout.module.css';
-
 export const SharedLayout = () => {
   return (
     <div>
